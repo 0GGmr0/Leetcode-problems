025dfdf4c96aed9b85c63df77ec3cd6cc1b61f48
0GGmr0 <18817619557@163.com>
2018-08-05 11:10:09 +0800

    9

---
+++ 9.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0 || (x != 0 && x % 10 == 0) ) return false;
+        int ans = 0;
+        while(ans < x) {
+            ans =ans * 10 + x % 10;
+            x /= 10;
+        }
+        return ans == x || x == ans / 10;
+    }
+}
